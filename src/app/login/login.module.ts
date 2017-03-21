621af41cb42a688463dadcd1f3c53312405220a8
Thierry Belliot <thierrybelliot@gmail.com>
2017-03-21 01:17:05 +0100

    add router

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { MdCardModule } from '@angular/material/card';
 import { MdButtonModule } from '@angular/material/button';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     MdCardModule,
-    MdButtonModule
+    MdButtonModule,
+    RouterModule,
+    FormsModule
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent, RegisterComponent]
 })
 export class LoginModule { }
